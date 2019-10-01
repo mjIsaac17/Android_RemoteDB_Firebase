7f0144b7a64cf0577702d6bb58e4e10172df8094
Isaac <isaac1998188@gmail.com>
2019-10-01 11:06:59 -0700

    Se agregan algunas dependencias de firebase, la clase Fireapp, y las referencias de los objetos de la UI al MainActivity

--- app/src/main/java/com/example/remotedb_firebase/MainActivity.java
+++ app/src/main/java/com/example/remotedb_firebase/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.remotedb_firebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etUser;
+    private EditText etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUser = (EditText)findViewById(R.id.etName);
+        etPassword = (EditText)findViewById(R.id.etPassword);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void StartLogin(){
+
     }
 }
