97550a89176c0d26189b71656c6227ae89803395
Isaac <isaac1998188@gmail.com>
2019-10-01 11:34:44 -0700

    Se agrega un listener para saber si el usuario ya esta autenticado y se redirecciona a nueva activity cuando ya se está

--- app/src/main/java/com/example/remotedb_firebase/MainActivity.java
+++ app/src/main/java/com/example/remotedb_firebase/MainActivity.java
@@ -1,35 +1,73 @@
 package com.example.remotedb_firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etUser;
+    private EditText etEmail;
     private EditText etPassword;
-    private Button btnLogin;
+    private Button btnSignIn;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etUser = (EditText)findViewById(R.id.etName);
-        etPassword = (EditText)findViewById(R.id.etPassword);
-        btnLogin = (Button)findViewById(R.id.btnLogin);
+        mAuth = FirebaseAuth.getInstance();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        etEmail = (EditText)findViewById(R.id.etEmail);
+        etPassword = (EditText)findViewById(R.id.etPassword);
+        btnSignIn = (Button)findViewById(R.id.btnSignIn);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() != null){
+                    startActivity(new Intent(MainActivity.this, AccountActivity.class));
+                }
+            }
+        };
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                StartSignIn();
             }
         });
     }
 
-    private void StartLogin(){
+    private void StartSignIn(){
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+            Toast.makeText(this, "No dejes campos vacíos", Toast.LENGTH_SHORT).show();
+            
+        }else{
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(MainActivity.this, "Error al registrarse", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
 
     }
 }
