3c1c45d70e979f10966702f088638790432d02e6
Isaac <isaac1998188@gmail.com>
2019-10-10 13:06:53 -0700

    Creación del activity para agregar nuevo planeta y se crea el diseño

--- app/src/main/java/com/example/remotedb_firebase/ListPlanets.java
+++ app/src/main/java/com/example/remotedb_firebase/ListPlanets.java
@@ -3,10 +3,16 @@ package com.example.remotedb_firebase;
 public class ListPlanets {
     private String head;
     private String description;
+    private String image;
 
-    public ListPlanets(String head, String description) {
+    public ListPlanets(String head, String description, String image) {
         this.head = head;
         this.description = description;
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
     }
 
     public String getHead() {
